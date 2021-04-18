a68e107678330225068e1099d182a069727e4fc2
Andy Schachter <andy.schachter@gmail.com>
2021-04-18 00:59:28 -0400

    created index.js added empty function and test array

---
+++ index.js
@@ -0,0 +1,26 @@
+/* eslint-disable no-console */
+const lineup = [{
+  id: 1, name: 'Chris Sale', position: 'P', teamId: 12, gameId: 123, salary: 9500
+}, {
+  id: 2, name: 'Yadier Molina', position: 'C', teamId: 22, gameId: 115, salary: 2500
+}, {
+  id: 3, name: 'Luke Voit', position: '1B', teamId: 20, gameId: 115, salary: 2800
+}, {
+  id: 4, name: 'Dee Gordon', position: '2B', teamId: 18, gameId: 101, salary: 3200
+}, {
+  id: 5, name: 'Manny Machado', position: '3B', teamId: 14, gameId: 134, salary: 3100
+}, {
+  id: 6, name: 'Troy Tulowitzki', position: 'SS', teamId: 27, gameId: 126, salary: 3300
+}, {
+  id: 7, name: 'Andrew McCutchen', position: 'OF', teamId: 11, gameId: 131, salary: 3800
+}, {
+  id: 8, name: 'Bryce Harper', position: 'OF', teamId: 15, gameId: 119, salary: 3800
+}, {
+  id: 9, name: 'Mookie Betts', position: 'OF', teamId: 12, gameId: 123, salary: 3600
+}]
+
+const validateLineup = (lineup) => {
+
+}
+
+module.exports = validateLineup
